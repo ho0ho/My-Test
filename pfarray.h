17878e7fe4266adf5a2d42f949d048491cde1407
ho0ho <eoa1256@nate.com>
2019-01-11 20:10:26 +0900

    pfarray_h 수정, 멤버함수 정의추가

--- pfarray.h
+++ pfarray.h
@@ -12,7 +12,17 @@ namespace pfaPARK {
     public:
         PFArray(int size = 50);
         PFArray(const PFArray<T>& other);
-        
+
+        void addElement(const T& element);
+        bool full() const;
+        void emptyArray();
+        int getCapacity() const;
+        int getUsed() const;
+
+        T& operator [] (int index);
+        PFArray<T>& operator = (const PFArray<T> rightSide);
+
+        ~PFArray();
     };
 } // pfaPARK
 
